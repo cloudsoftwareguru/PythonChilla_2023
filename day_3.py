2aae73e3b865977190f595725dc7a9ee12fdc995
Tahir Faqir <tahir.f@evolvingsol.com>
2023-01-20 11:18:39 -0600

    new day 13

--- day_3.py
+++
@@ -1,5 +0,0 @@
-print("Hello World!")
-
-the_name = input("Please enter your name: ")
-
-print("You entered your name as: ", the_name)
---
+++ day_6/day_3.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+excel_data_df = pd.read_excel('pandas_01_day6.xlsx')#, sheet_name='Form Responses 1')
